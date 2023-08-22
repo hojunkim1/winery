54a6c10a5872d8b58b5cff95c655490f74721357
rretjin <yelim0s@naver.com>
2023-08-22 15:15:40 +0900

    recyclerView

--- app/src/main/java/com/example/winery/ui/search/SearchFragment.kt
+++ app/src/main/java/com/example/winery/ui/search/SearchFragment.kt
@@ -4,8 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
+import android.widget.EditText
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.winery.MainActivity
+import com.example.winery.R
 import com.example.winery.databinding.FragmentSearchBinding
 
 class SearchFragment : Fragment() {
@@ -18,9 +22,20 @@ class SearchFragment : Fragment() {
     ): View {
         binding = FragmentSearchBinding.inflate(inflater)
         binding.toolbarSearch.title = "Search"
+        binding.toolbarSearch.inflateMenu(R.menu.search_menu)
         binding.toolbarSearch.setNavigationOnClickListener {
             val act = activity as MainActivity
             act.supportFragmentManager.popBackStack()
+        }
+        binding.toolbarSearch.setOnMenuItemClickListener{
+            when (it.itemId) {
+                R.id.search_menu_search -> {
+                    val action = SearchFragmentDirections.actionNavigationSearchFragmentToSearchListFragment()
+                    findNavController().navigate(action)
+                    true
+                }
+                else -> false
+            }
         }
         return binding.root
     }
